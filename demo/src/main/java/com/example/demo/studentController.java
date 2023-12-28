131473165658550972f9f6cb3ad9bf8f765d3723
AnilChaudhary <anil.bhumal@gmail.com>
2023-12-29 00:22:21 +0545

    Spring Boot

--- demo/src/main/java/com/example/demo/studentController.java
+++ demo/src/main/java/com/example/demo/studentController.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +15,29 @@ import org.springframework.web.bind.annotation.RestController;
 public class studentController {
 	
 	//http://localhost:8080/student
+	
+	
 	@GetMapping("/student")
-	public Student getStudent() {
+	public Student getStudent() { //java rest api which returns java bean/object to client
 		return new Student("Anil","Chaudhary");
+	}
+	
+	@GetMapping("/students")
+	public List<Student> getStudent1(){
+		List<Student> student1=new ArrayList<>();
+		student1.add(new Student("Anil","Chaudhary"));
+		student1.add(new Student("Banil","Chaudhary"));
+		student1.add(new Student("Canil","Chaudhary"));
+		
+		return student1;
+	}
+	
+	//http://localhost:8080/student/Anil/Chaudhary
+	//@PathVariable annotation
+	@GetMapping("/student/{firstName}/{lastName}")
+	public Student studentPathVariable(@PathVariable("firstName") String firstName,
+			@PathVariable("lastName") String lastName) {
+		return new Student(firstName,lastName);
 	}
 
 }
